8d63a53c554012d13239e8b6ace88b0be716d0ea
Adam Skoczeń <adamsk91@wp.pl>
2023-02-12 09:52:30 +0100

    some name changes have been made

--- src/models/modal-movie.js
+++ src/models/modal-movie.js
@@ -1,16 +1,19 @@
-const closeBtn = document.querySelector('.Modal__Close');
-const modal = document.querySelector('.Modal');
-const blur = document.querySelector('.blur');
+const blur = document.querySelector('[data-modal="blur"]');
+const modalOpen = document.querySelector('[data-modal="open"]');
+const closeBtn = document.querySelector('[data-modal="close"]');
 const movieCard = document.querySelectorAll('.movie-card');
 
+console.log(modalOpen);
+
 const toggleHidden = () => {
-  [blur, modal].map(el => el.classList.toggle('hidden'));
+  [modalOpen, blur].map(el => el.classList.toggle('hidden'));
+  console.log("Otwarcie / zamknięcie modala");
 };
 
 [closeBtn, blur, ...movieCard].map(el => el.addEventListener('click', toggleHidden));
 
 const escModal = e => {
-  if (e.key === 'Escape') [blur, modal].map(el => el.classList.add('hidden'));
+  if (e.key === 'Escape') [modalOpen, blur].map(el => el.classList.add('hidden'));
 };
 
 document.addEventListener('keyup', escModal);
