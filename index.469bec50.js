eef166778d7fc9ebdedc0b0820b54924807ad25c
Hubuuus <111136742+Hubuuus@users.noreply.github.com>
2023-02-13 18:53:53 +0000

    Deploying to gh-pages from @ Hubuuus/group-9-project.js@4f2d319960a7bf8a52cebbe5b2d00d13b503434c 🚀

--- index.469bec50.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".Modal__Button--Watched"),document.querySelector(".Modal__Button--Queue"),document.querySelector(".gallery");
-//# sourceMappingURL=index.469bec50.js.map
---
+++ index.c88d7cc1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".Modal__Button--Watched"),t=(document.querySelector(".Modal__Button--Queue"),document.querySelector(".gallery"));e.addEventListener("click",(function(){const e=document.createElement("div");t.appendChild(e),e.innerHTML="created div ",localStorage.setItem("testDiv",e.outerHTML),console.log("div added to local storage")}));
+//# sourceMappingURL=index.c88d7cc1.js.map
