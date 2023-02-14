0c2cf9586efc387a2f744b72f06c8f816f8b616c
agata-zybula <agata.masiewicz@gmail.com>
2023-02-14 15:38:06 +0100

    update

--- src/models/modal-footer.js
+++ src/models/modal-footer.js
@@ -1,14 +1,33 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const modal = document.querySelector('.ModalFooter');
+const overlay = document.querySelector('.OverlayFooter');
+const openModalBtn = document.querySelector('.ButtonOpen');
+const closeModalBtn = document.querySelector('.ModalFooter__ButtonClose');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const openModal = function () {
+  modal.classList.remove('IsHidden');
+  overlay.classList.remove('IsHidden');
+};
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+openModalBtn.addEventListener('click', openModal);
+
+const closeModal = function () {
+  modal.classList.add('IsHidden');
+  overlay.classList.add('IsHidden');
+};
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('IsHidden')) {
+    closeModal();
   }
-})();
+});
+
+if (
+  modal.classList.contains('IsHidden') ||
+  overlay.classList.contains('IsHidden')
+) {
+} else {
+  closeModalBtn.removeEventListener();
+}
