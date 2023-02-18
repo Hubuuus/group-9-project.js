08de314cbaed61f223464647c12a4dbfdb9ccca3
Michał Masiulanis <m.masiulanis@gmail.com>
2023-02-18 20:38:50 +0100

    pagination html base

--- src/models/pagination.js
+++ src/models/pagination.js
@@ -1,4 +1,7 @@
-let page = 1;
+import axios from 'axios';
+
+// let page = 1;
+const API_KEY = '28e7de8a02a020e11a900cecedfaedb8';
 
 // SMALL FUNCTION CONVERTING RESPONSE INTO JSON
 function fetchJsonResponse(url) {
@@ -6,8 +9,19 @@ function fetchJsonResponse(url) {
     .then(response => response.json())
     .catch(error => console.log('error', error));
 }
+////////////////////////////////////////////////////////
+// TEST
+
+// fetchJsonResponse(
+//   `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+// )
+//   .then(response => console.log(response))
+//   .catch(error => console.log(error));
+
+////////////////////////////////////////////////////////
+
 // FUNCTION HANDLING PAGINATION DIV UNDER MOVIE GALLERY
-function renderPaginator(count, selectedPage = 1, pageSize = 12) {
+export async function renderPaginator(count, selectedPage = 1, pageSize = 12) {
   const pages = Math.ceil(count / pageSize);
   const select = document.getElementById('Pagination-Select');
   select.innerHTML = '';
@@ -22,14 +36,27 @@ function renderPaginator(count, selectedPage = 1, pageSize = 12) {
     select.append(option);
   }
   select.addEventListener('change', event => {
+    const selectedPage = event.target.value;
+    const loadingSpinner = document.querySelector('.Pagination-Loader');
+    loadingSpinner.classList.remove('hidden');
+
+    const response = axios.get(
+      'https://api.themoviedb.org/3/trending/movie/day',
+      { params: { api_key: API_KEY, page: 1 } }
+    );
     fetchJsonResponse(
       `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${selectedPage}`
-    ).then(response => {
-      //rekurencja - wywoływanie funkcji wewnątrz siebie)
-      renderPaginator(response.data.total_results, selectedPage);
-      // presentMovies();
-    });
+    )
+      .then(response => {
+        //rekurencja - wywoływanie funkcji wewnątrz siebie)
+        renderPaginator(response.data.total_results, selectedPage);
+        // presentMovies();
+      })
+      .then(() => {
+        setTimeout(() => {
+          loadingSpinner.classList.add('hidden');
+        }, 1500);
+      });
   });
-  renderPaginator(response.data.total_results);
 }
 // document.addEventListener('DOMContentLoaded', renderPaginator());
