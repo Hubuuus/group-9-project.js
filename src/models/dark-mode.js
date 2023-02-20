509e5367de0007d0b272391eb593d702db971340
Adam Skoczeń <adamsk91@wp.pl>
2023-02-20 22:28:13 +0100

    fixed dark-mode, text animation

--- src/models/dark-mode.js
+++ src/models/dark-mode.js
@@ -1,20 +1,34 @@
 const container = document.querySelector('.Container');
 const textColor = document.querySelectorAll('.dark-mode');
 const darkModeBtn = document.querySelectorAll('.dark-mode-btn');
+const lightBtn = document.querySelector('.light');
+const darkBtn = document.querySelector('.dark');
 
-console.log(textColor);
+let darkModeFlag = JSON.parse(localStorage.getItem('darkModeFlag')) || false;
 
-const darkMode = () => {
-    if (container.style.backgroundColor === "") {
-        container.style.backgroundColor = "black";
-        textColor.forEach(el => el.style.color = "white");
-    }
-    else {
-        container.style.backgroundColor = "";
-        textColor.forEach(el => el.style.color = "");
-    }
-    darkModeBtn.forEach(el => el.classList.toggle("dark-hidden"));
-    console.log("klik!")
-}
+const setDarkMode = (flag) => {
+    if (flag) {
+    darkBtn.classList.add("dark-hidden");
+    lightBtn.classList.remove("dark-hidden");
+    container.style.backgroundColor = "#333";
+    textColor.forEach(el => el.style.color = "#ddd");
+    textColor.forEach(el => el.style.backgroundColor = "#333");
+    } else {
+    darkBtn.classList.remove("dark-hidden");
+    lightBtn.classList.add("dark-hidden");
+    container.style.backgroundColor = "";
+    textColor.forEach(el => el.style.color = "");
+    textColor.forEach(el => el.style.backgroundColor = "");
+  }
+    darkModeFlag = flag;
+  localStorage.setItem('darkModeFlag', JSON.stringify(flag));
+};
 
-darkModeBtn.forEach(el => el.addEventListener('click', darkMode));
+darkModeBtn.forEach(el => {
+    el.addEventListener('click', () => {
+    container.classList.add("transitionEfc")
+      setDarkMode(!darkModeFlag);
+  });
+});
+
+setDarkMode(darkModeFlag);
