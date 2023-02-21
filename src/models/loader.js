9c60f3649fc005843a11bf2377f87bd4ce4a5e2f
Hubert Dulas <111136742+Hubuuus@users.noreply.github.com>
2023-02-21 23:43:43 +0100

    fix dev

--- src/models/loader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    const loader = document.querySelector('.Loader');
-  
-    loader.classList.add('LoaderHidden');
-  
-    loader.addEventListener('transitionend', () => {
-      loader.innerHTML = '';
-    });
-  });
